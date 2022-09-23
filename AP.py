b4fdea55db51a3ba92d43dcd8a03190ea3748f9c
Heblg <110850858+Heblg@users.noreply.github.com>
2022-09-23 13:58:00 -0300

    Update AP.py

--- AP.py
+++ AP.py
@@ -1,47 +1,57 @@
-l, x, y, z = [], 0, 0, 0
+l, x, y, z = [],0,0,0
 while True:
-    an = input("AN: ")
-    a1 = input("A1: ")
-    r = input("R: ")
-    n = input("N: ")
+    an = input("Last value: ")
+    a1 = input("First value: ")
+    r = input("Ratio: ")
+    n = input("Number of values: ")
 
-    if a1 == "x":
-        an,n,r = float(an),int(n),float(r)
-        x = r * (n - 1)
-        y = an
-        an -= x
-        x = z
-        for i in range(n):
-            l.append(an)
-            an += r
-        print(l, "\n", y, "\n")
-        print("SN:", sum(l), "\n")
+    if a1 == "X" or a1 == "x":
+        an, n, r = float(an),int(n),float(r)
+        a1 = an - (n-1) * r
+        x = a1
 
-    if an == "x":
-        a1,n,r = float(a1),int(n),float(r),
-        z,y = a1,n
-        a1 = a1 - r
-        for i in range(n):
-            a1 = a1 + r
+        while a1 <= an:
             l.append(a1)
-        print(l, "\n", a1, "\n")
-        print("SN:", sum(l), "\n")
+            a1 += r
 
-    if n == "x":
-        an,r,a1 = float(an),float(r),float(a1),
+        print(f'{l} \n{x} \n')
+        print(f'Sum: {sum(l)} \n')
+
+    if an == "X" or an == "x":
+        a1, n, r = float(a1), int(n), float(r)
+        z, y = a1, n
+
+        while n != 0:
+            l.append(a1)
+            a1 += r
+            n -= 1
+
+        print(f'{l} \n{a1-r} \n')
+        print(f'Sum: {sum(l)} \n')
+
+    if n == "X" or n == "x":
+        a1, an, r = float(a1), float(an), float(r)
         y = an
         an = ((an - a1) - (r * -1))/r
         x = an.__trunc__()
-        for i in range(x):
+
+        while x != 0:
             l.append(a1)
             a1 += r
-        print(l, "\n", an, "\n")
-        print("SN:", sum(l), "\n")
+            x -= 1
 
-    if r == "x":
-        an,n,a1 = float(an),int(n),float(a1)
-        x = (an - a1) / (n - 1)
-        for i in range(n):
+        print(f'{l} \n{an} \n')
+        print(f'Sum: {sum(l)} \n')
+
+
+    if r == "X" or r == "x":
+        a1, an, n = float(a1), float(an), int(n)
+
+        r = (an - a1) / (n-1)
+        while n > 0:
             l.append(a1)
-            a1 += x
-        print(l, "\n", sum(l), "\n")
+            a1 += r
+            n -= 1
+
+        print(f'{l} \n{r} \n')
+        print(f'Sum: {sum(l)} \n')
